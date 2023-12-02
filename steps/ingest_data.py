9ab7ece1651f45ac7679866eaa04f56324131e6d
Barryibs <bis.ibrahima@gmail.com>
2023-12-01 19:09:47 -0500

    Version1.0 of Zenml pipeline

--- steps/ingest_data.py
+++ steps/ingest_data.py
@@ -15,22 +15,22 @@ class IngestData:
         logging.info(f'Ingesting data from {self.data_path}')
         return pd.read_csv(self.data_path)
     
-    @step
-    def ingest_df(data_path: str) -> pd.DataFrame:
-        """""
-        Ingesting the data from the data_path
-        
-        Args:
-            data_path: path to the data
-        returns:
-            pd.DataFrame: the ingested data
-        """""
-        
-        try:
-            ingest_data = IngestData(data_path)
-            df = ingest_data.get_data()
-            return df
-        except Exception as e:
-            logging.error(f'Error while ingesting data: {e}')
-            raise e
+@step
+def ingest_df(data_path: str) -> pd.DataFrame:
+    """""
+    Ingesting the data from the data_path
+    
+    Args:
+        data_path: path to the data
+    returns:
+        pd.DataFrame: the ingested data
+    """""
+    
+    try:
+        ingest_data = IngestData(data_path)
+        df = ingest_data.get_data()
+        return df
+    except Exception as e:
+        logging.error(f'Error while ingesting data: {e}')
+        raise e
     
